a20d19c341879ba1aa4cec2293c0901ffc46131a
Marcos Pereira <3464445+marcospgp@users.noreply.github.com>
2024-04-08 17:18:10 +0100

    .

--- _plugins/header-id-hierarchy.rb
+++ _plugins/header-id-hierarchy.rb
@@ -1,12 +1,18 @@
-# Hierarchical ID Generator - Written by ChatGPT
-# Generates unique hierarchical IDs for headers to ensure anchor links work correctly,
-# even with duplicate header names.
+# Hierarchical ID generator - written by ChatGPT
+#
 # Example:
+#
 # # Drinks          -> id="drinks"
 # ## Coffee         -> id="drinks--coffee"
 # ### Latte         -> id="drinks--coffee--latte"
 # ### Espresso      -> id="drinks--coffee--espresso"
 # ## Tea            -> id="drinks--tea"
+#
+# Also updates links, such as from table of contents:
+#
+# <a href="#coffee">Coffee</a>
+# becomes:
+# <a href="#drinks--coffee">Coffee</a>
 
 # Custom implementation of `parameterize` for strings
 class String
@@ -17,9 +23,10 @@ class String
 end
 
 Jekyll::Hooks.register [:pages, :documents], :post_render do |doc|
-
   current_hierarchy = {}
+  header_id_map = {}
 
+  # Update header IDs with hierarchical structure
   modified_content = doc.output.gsub(/<(h[1-6])(.*?)>(.*?)<\/\1>/) do |match|
     level = $1[1].to_i  # Extract the numerical level of the header
     content = $3.strip
@@ -34,8 +41,23 @@ Jekyll::Hooks.register [:pages, :documents], :post_render do |doc|
     # Construct the hierarchical ID by concatenating parent IDs
     hierarchical_id = current_hierarchy.values.join("--")
 
+    # Map original ID to new hierarchical ID for later link updates
+    header_id_map[sanitized_id] = hierarchical_id
+
     # Reconstruct the header tag with the new hierarchical ID
     "<#{$1} id=\"#{hierarchical_id}\">#{content}</#{$1}>"
+  end
+
+  # Update links in the document to reflect the new hierarchical header IDs
+  modified_content.gsub!(/<a href="#([^"]+)">/) do |link_match|
+    original_id = $1
+    if header_id_map.key?(original_id)
+      # Replace link with updated hierarchical ID if it exists in the map
+      link_match.sub("##{original_id}", "##{header_id_map[original_id]}")
+    else
+      # No change if the ID isn't in the header map
+      link_match
+    end
   end
 
   # Update the document's output with modified content
