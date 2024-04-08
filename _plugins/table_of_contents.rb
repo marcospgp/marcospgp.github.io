7242030331f9ff5a02818e64e8b4520a760fb85c
Marcos Pereira <3464445+marcospgp@users.noreply.github.com>
2024-04-08 13:56:20 +0100

    .

--- _plugins/table_of_contents.rb
+++ _plugins/table_of_contents.rb
@@ -4,42 +4,36 @@
 #
 #   <article class="table-of-contents">
 #     {% if page.table_of_contents != false %}
-#       {{ page.table_of_contents | safe }}
+#       {{ content | table_of_contents | safe }}
 #     {% endif %}
 #   </article>
 #
-# The | safe filter in Liquid tells Jekyll that the TOC HTML is safe to render
-# as-is, without escaping it.
-#
-# Example TOC structure for a document with the following headers:
-#
-#   # Drinks          -> id="drinks"
-#   ## Coffee         -> id="drinks--coffee"
-#   ### Latte         -> id="drinks--coffee--latte"
-#   ### Espresso      -> id="drinks--coffee--espresso"
-#   ## Tea            -> id="drinks--tea"
-#
-# Would result in a TOC like:
-#
-#   - [Drinks](#drinks)
-#     - [Coffee](#drinks--coffee)
-#       - [Latte](#drinks--coffee--latte)
-#       - [Espresso](#drinks--coffee--espresso)
-#     - [Tea](#drinks--tea)
+# The "safe" filter tells Jekyll that the TOC HTML is safe to render without
+# escaping.
 
-Jekyll::Hooks.register [:pages, :documents], :pre_render do |doc|
-  toc = "<ul>"
-  headers_found = false
+module Jekyll
+  module TocFilter
+    def table_of_contents(input)
+      toc = "<ul>"
+      headers_found = false
 
-  doc.content.scan(/^(\#{1,6})\s+(.+)$/).each do |match|
-    headers_found = true
-    level, title = match
-    header_level = level.length
-    sanitized_title = title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
-    toc << "<li class=\"toc-level-#{header_level}\"><a href=\"##{sanitized_title}\">#{title}</a></li>\n"
+      input.scan(/^(\#{1,6})\s+(.+)$/).each do |match|
+        headers_found = true
+        level, title = match
+        header_level = level.length
+        sanitized_title = title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+        toc << "<li class=\"toc-level-#{header_level}\"><a href=\"##{sanitized_title}\">#{title}</a></li>\n"
+      end
+
+      toc << "</ul>"
+
+      if headers_found
+        toc
+      else
+        "No headers found for TOC."
+      end
+    end
   end
-
-  toc << "</ul>"
-
-  doc.data['table_of_contents'] = toc if headers_found
 end
+
+Liquid::Template.register_filter(Jekyll::TocFilter)
