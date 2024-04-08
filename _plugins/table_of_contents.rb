e6797941e6f84aa73b6ea0f89076d4bce0058548
Marcos Pereira <3464445+marcospgp@users.noreply.github.com>
2024-04-08 15:48:12 +0100

    .

--- _plugins/table_of_contents.rb
+++ _plugins/table_of_contents.rb
@@ -13,37 +13,34 @@
 
 module Jekyll
   module TocFilter
-    # Custom implementation of `parameterize` for strings
     def parameterize(str, separator = '-')
       str.downcase.gsub(/[^a-z0-9]+/i, separator).chomp(separator)
     end
 
-    # Method to generate hierarchical IDs for headers
-    def generate_hierarchical_id(header_content, hierarchy)
-      sanitized_id = parameterize(header_content)
-      hierarchy.join("--") + "--#{sanitized_id}"
-    end
-
-    # Method to generate the table of contents (TOC) from the document's content
     def table_of_contents(content)
       toc = ""
       headers_found = false
       hierarchy = []
 
-      # Regex to match HTML headers
-      content.scan(/<(h[2-6])(.*?)>(.*?)<\/\1>/).each do |match|
+      content.scan(/<(h[2-6])[^>]*>(.*?)<\/\1>/).each do |match|
         headers_found = true
-        tag, _, title = match
-        header_level = tag[1].to_i
+        level = match[0][1].to_i
+        title = match[1].strip
+        sanitized_id = parameterize(title)
 
         # Update the hierarchy based on the current header level
-        hierarchy = hierarchy[0, header_level - 2]  # Adjust to exclude h1
-        hierarchical_id = generate_hierarchical_id(title, hierarchy)
+        if level == 2
+          hierarchy = []
+        elsif level > hierarchy.length + 2
+          hierarchy = hierarchy[0, level - 2]
+        end
+        hierarchy << sanitized_id
 
-        # Construct the hierarchical structure for the TOC
-        toc << "<li><a href=\"##{hierarchical_id}\">#{title}</a>"
-        toc << "<ul>" if header_level < 6  # Add sub-list if not h6
-        toc << "</li>\n"
+        # Generate the hierarchical ID for the header
+        hierarchical_id = hierarchy.join("--")
+
+        # Append the TOC entry for the header
+        toc << "<li><a href=\"##{hierarchical_id}\">#{title}</a></li>\n"
       end
 
       if headers_found
