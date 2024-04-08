c7e9e550a6a9c99fcd7942e0c2aff48605058576
Marcos Pereira <3464445+marcospgp@users.noreply.github.com>
2024-04-08 13:50:38 +0100

    .

--- _plugins/table_of_contents.rb
+++ _plugins/table_of_contents.rb
@@ -1,14 +1,15 @@
 # Hierarchical TOC Generator - Written by ChatGPT
-# Sets 'table_of_contents' variable for Jekyll documents with the Markdown-formatted TOC.
-# The 'table_of_contents' variable is accessible in templates, and its display is controlled by front matter.
 #
 # Usage in templates:
 #
 #   <article class="table-of-contents">
 #     {% if page.table_of_contents != false %}
-#       {{ page.table_of_contents | markdownify }}
+#       {{ page.table_of_contents | safe }}
 #     {% endif %}
 #   </article>
+#
+# The | safe filter in Liquid tells Jekyll that the TOC HTML is safe to render
+# as-is, without escaping it.
 #
 # Example TOC structure for a document with the following headers:
 #
@@ -28,23 +29,17 @@
 
 Jekyll::Hooks.register [:pages, :documents], :pre_render do |doc|
   toc = "<ul>"
-  headers_found = 0
+  headers_found = false
 
-  # Regex to match Markdown headers based on the number of '#' characters
-  # This will capture the header level and the header text
   doc.content.scan(/^(\#{1,6})\s+(.+)$/).each do |match|
+    headers_found = true
     level, title = match
-    header_level = level.length  # Determine header level from number of '#' characters
-    sanitized_title = title.gsub(/[^\w\s-]/, '').strip.downcase.gsub(/\s+/, '-')  # Basic sanitization and slugification
-
-    toc << "<li class=\"toc-level-#{header_level}\"><a href=\"##{sanitized_title}\">#{title.strip}</a></li>\n"
-    headers_found += 1
+    header_level = level.length
+    sanitized_title = title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+    toc << "<li class=\"toc-level-#{header_level}\"><a href=\"##{sanitized_title}\">#{title}</a></li>\n"
   end
 
   toc << "</ul>"
 
-  # Only set the TOC data if headers were found
-  if headers_found > 0
-    doc.data['table_of_contents'] = toc
-  end
+  doc.data['table_of_contents'] = toc if headers_found
 end
