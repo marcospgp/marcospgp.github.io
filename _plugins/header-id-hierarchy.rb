0beb457878f3174b491c28079bd9123cb0492da9
Marcos Pereira <3464445+marcospgp@users.noreply.github.com>
2024-04-14 21:56:10 +0100

    Update header-id-hierarchy.rb

--- _plugins/header-id-hierarchy.rb
+++ _plugins/header-id-hierarchy.rb
@@ -20,28 +20,60 @@
 module Jekyll
   Jekyll::Hooks.register [:pages, :documents], :post_render do |document|
     content = document.output
-    new_content = ''
-    inside_anchor = false
-    tag_depth = 0
 
-    content.scan(/<[^>]+>|[^<]+/).each do |fragment|
-      if fragment.start_with?('<')
-        if fragment.start_with?('<a ')
-          inside_anchor = true
-          tag_depth += 1
-        elsif fragment.start_with?('</a>')
-          tag_depth -= 1
-          inside_anchor = false if tag_depth == 0
-        elsif fragment.start_with?('<img ') && !inside_anchor
-          src_match = fragment.match(/src=['"]([^'"]*)['"]/)
-          src = src_match[1] if src_match
-          fragment = "<a href='#{src}' target='_blank'>#{fragment}</a>"
+    # Simplified parser to build a tree structure
+    def build_tree(html)
+      tree = { tag: :root, children: [] }
+      current_node = tree
+
+      html.scan(/<(\w+)|<\/(\w+)>|([^<>]+)/) do |open_tag, close_tag, text|
+        if open_tag
+          new_node = { tag: open_tag.to_sym, children: [], parent: current_node }
+          current_node[:children] << new_node
+          current_node = new_node
+        elsif close_tag
+          current_node = current_node[:parent]
+        elsif text.strip != ''
+          current_node[:children] << { tag: :text, content: text }
         end
       end
 
-      new_content += fragment
+      tree
     end
 
-    document.output = new_content
+    # Traverser that ignores <a> tags and processes <img> tags
+    def process_tree(node)
+      return if node[:tag] == :a
+
+      node[:children].each do |child|
+        if child[:tag] == :img
+          src = child[:attributes][:src]
+          child[:parent][:children] << { tag: :a, attributes: { href: src, target: '_blank' }, children: [child] }
+          child[:parent][:children].delete(child)
+        else
+          process_tree(child) if child[:children]
+        end
+      end
+    end
+
+    tree = build_tree(content)
+    process_tree(tree)
+
+    # Render tree back to HTML
+    document.output = render_html(tree)
+  end
+
+  def render_html(node)
+    # Simplified rendering of the tree back to HTML
+    html = ''
+    node[:children].each do |child|
+      if child[:tag] == :text
+        html += child[:content]
+      else
+        inner_html = render_html(child)
+        html += "<#{child[:tag]} #{child[:attributes].map{|k,v| "#{k}='#{v}'"}.join(' ')}>#{inner_html}</#{child[:tag]}>"
+      end
+    end
+    html
   end
 end
